215130d0a5c7610fc01f8fe14f943cf193565691
Josh-McFarlin <jmcfarlin6@gatech.edu>
2018-01-15 07:20:06 -0500

    Add updated website

---
+++ projects/projects.js
@@ -0,0 +1,77 @@
+let projectsJSON = [
+    {
+        "Title": "AskBuzz",
+        "Description": "An Alexa skill for use by Georgia Tech students. This app was written by Josh McFarlin, Ryan Miles, and Dylan Nektalov during HackGT 2017.",
+        "Images": [
+            "projects/images/dot.jpg",
+        ],
+        "Link": ""
+    },
+    {
+        "Title": "AstroIDE",
+        "Description": "An IDE with syntax highlighting support for over 110 languages. Written in Java utilizing JavaFX and Ace.js for syntax highlighting.",
+        "Images": [
+            "projects/images/AstroIDEMain.png",
+            "projects/images/AstroIDESearch.png"
+        ],
+        "Link": "https://github.com/Josh-McFarlin/AstroIDE"
+    },
+    {
+        "Title": "CryptoCompareAPI",
+        "Description": "Java implementation of the CryptoCompare API service.",
+        "Images": [
+            "projects/images/cryptocompare.png"
+        ],
+        "Link": "https://github.com/Josh-McFarlin/CryptoCompareAPI"
+    },
+    {
+        "Title": "GraffitiMap",
+        "Description": "An Alexa skill for use by Georgia Tech students. This app was written by Josh McFarlin, Ryan Miles, and Dylan Nektalov during HackGT 2017.",
+        "Images": [
+            "projects/images/GraffitiMapMain.png",
+            "projects/images/GraffitiMapSubmit.png",
+            "projects/images/GraffitiMapAdmin.png"
+        ],
+        "Link": "https://github.com/Josh-McFarlin/GraffitiMap"
+    },
+    {
+        "Title": "PyWeatherService",
+        "Description": "This code is a proof of concept SMS weather information system written in Python 3.",
+        "Images": [
+            "projects/images/weather.png"
+        ],
+        "Link": "https://github.com/Josh-McFarlin/PyWeatherService"
+    },
+    {
+        "Title": "Programming Based 3D Modeling iOS App",
+        "Description": "Programming Based 3D Modeling iOS App",
+        "Images": [
+            "projects/images/ProgrammingAppViewer.png",
+            "projects/images/ProgrammingAppEditor.png",
+            "projects/images/ProgrammingAppHelp.png"
+        ],
+        "Link": ""
+    },
+    {
+        "Title": "This Website",
+        "Description": "I created this website entirely by myself. It is the second iteration of my website, you can find the first iteration on my GitHub.",
+        "Images": [
+            "projects/images/WebStorm.png",
+            "projects/images/HomeProgramming.png"
+        ],
+        "Link": ""
+    },
+    {
+        "Title": "3D Scanning of Brains and Soft Tissue",
+        "Description": "Conducted at the Evolutionary Neuroscience Laboratory at George Washington University. Developed a procedure for creating 3D models of brains and soft tissue using photogrammetry. Documented findings in a research paper now used by graduate and post-graduate students to aid in the preservation of specimen and research.",
+        "Images": [
+            "projects/images/brain1.jpg",
+            "projects/images/brain2.jpg",
+            "projects/images/brain3.jpg",
+            "projects/images/brain4.jpg",
+            "projects/images/brain5.jpg",
+            "projects/images/brain6.jpg"
+        ],
+        "Link": ""
+    }
+];
---
+++ static/js/portfolio.js
@@ -0,0 +1,136 @@
+class ProjectHolder {
+    constructor() {
+        if(window.innerHeight > window.innerWidth){
+            this.projectPerRow = 2;
+        } else {
+            this.projectPerRow = 4;
+        }
+        this.rows = 0;
+        this.lastRowProjects = 0;
+        this.numProjects = 0;
+        this.htmlSelf = document.getElementById("projectHolder");
+        this.makeRow()
+    }
+
+    makeRow() {
+        let row = document.createElement('div');
+        row.className = 'projectRow';
+
+        let projectsDiv = document.createElement('div');
+        projectsDiv.className = 'projectRowProjects';
+        row.appendChild(projectsDiv);
+
+        let expandingDiv = document.createElement('div');
+        expandingDiv.className = 'expandingDiv';
+        row.appendChild(expandingDiv);
+
+        let titleCloseDiv = document.createElement('div');
+        titleCloseDiv.className = 'titleCloseDiv';
+        expandingDiv.appendChild(titleCloseDiv);
+
+        let titleElem = document.createElement('h1');
+        titleElem.setAttribute("class", "expandingDivTitle");
+        titleElem.appendChild(document.createTextNode("Title"));
+        let titleDiv = document.createElement('div');
+        titleDiv.className = 'title';
+        titleDiv.appendChild(titleElem);
+        titleCloseDiv.appendChild(titleDiv);
+
+        let closeElem = document.createElement('h3');
+        closeElem.setAttribute("class", "closeElem");
+        closeElem.appendChild(document.createTextNode("×"));
+        let closeDiv = document.createElement('div');
+        closeDiv.className = 'closeDiv';
+        closeDiv.appendChild(closeElem);
+        titleCloseDiv.appendChild(closeDiv);
+
+        closeDiv.onclick = function() {
+            let allProjects = document.getElementsByClassName('project');
+            for (let i = 0; i < allProjects.length; i++) {
+                allProjects[i].classList.add('hide-after','hide-before');
+            }
+
+            let allExpanding = document.getElementsByClassName('expandingDiv');
+            for (let i = 0; i < allExpanding.length; i++) {
+                allExpanding[i].style.display = "none";
+            }
+        };
+
+        let descImg = document.createElement('div');
+        descImg.className = 'descImg';
+
+        let descElem = document.createElement('p');
+        descElem.setAttribute("class", "expandingDivDesc");
+        descElem.appendChild(document.createTextNode("Description"));
+        let descDiv = document.createElement('div');
+        descDiv.className = 'description';
+        descDiv.appendChild(descElem);
+        descImg.appendChild(descDiv);
+
+        let imgElem = document.createElement('img');
+        imgElem.setAttribute("class", "expandingDivImg");
+        imgElem.setAttribute('src', 'projects/images/project.jpg');
+        let imgDiv = document.createElement('div');
+        imgDiv.className = 'image';
+        imgDiv.appendChild(imgElem);
+        descImg.appendChild(imgDiv);
+
+        expandingDiv.appendChild(descImg);
+
+        this.htmlSelf.appendChild(row);
+        this.rows++;
+    }
+
+    makeProject(title, description, imageSource) {
+        if (this.lastRowProjects >= this.projectPerRow) {
+            this.makeRow();
+            this.lastRowProjects = 0;
+        }
+
+        this.numProjects++;
+
+        let projectDiv = document.createElement('div');
+        projectDiv.className = 'project';
+        projectDiv.style.backgroundImage = "url(" + imageSource + ")";
+        projectDiv.style.backgroundPosition = "center";
+        projectDiv.style.backgroundSize = "cover";
+        projectDiv.dataset.row = this.rows - 1;
+        projectDiv.classList.add('hide-after','hide-before');
+        projectDiv.onclick = function() {
+            let allProjects = document.getElementsByClassName('project');
+            for (let i = 0; i < allProjects.length; i++) {
+                allProjects[i].classList.add('hide-after','hide-before');
+            }
+
+            let allExpanding = document.getElementsByClassName('expandingDiv');
+            for (let i = 0; i < allExpanding.length; i++) {
+                allExpanding[i].style.display = "none";
+            }
+
+            projectDiv.classList.remove('hide-after','hide-before');
+            document.getElementsByClassName('expandingDiv').item(projectDiv.dataset.row).style.display = "flex";
+            document.getElementsByClassName('expandingDivTitle').item(projectDiv.dataset.row).innerHTML = title;
+            document.getElementsByClassName('expandingDivDesc').item(projectDiv.dataset.row).innerHTML = description;
+            document.getElementsByClassName('expandingDivImg').item(projectDiv.dataset.row).src = imageSource;
+        };
+
+        let titleElem = document.createElement('p');
+        titleElem.appendChild(document.createTextNode(title));
+        let titleDiv = document.createElement('div');
+        titleDiv.className = 'titleDiv';
+        titleDiv.appendChild(titleElem);
+        projectDiv.appendChild(titleDiv);
+
+        document.getElementsByClassName('projectRowProjects')[this.rows - 1].appendChild(projectDiv);
+        this.lastRowProjects++;
+    }
+}
+
+$(document).ready(function() {
+    let projectHolder = new ProjectHolder();
+
+    console.log(projectsJSON);
+    for (let i = 0; i < projectsJSON.length; i++) {
+        projectHolder.makeProject(projectsJSON[i].Title, projectsJSON[i].Description, projectsJSON[i].Images[0]);
+    }
+});
