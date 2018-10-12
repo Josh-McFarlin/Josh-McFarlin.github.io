5df2131674a03baaf595554b3f73602cfbffc766
Josh <jmcfarlin6@gatech.edu>
2018-10-12 04:11:45 -0400

    Simplified routing and removed 404 to hopefully fix conflicting Github pages

--- src/index.js
+++ src/index.js
@@ -1,7 +1,60 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import {createMuiTheme, MuiThemeProvider} from "@material-ui/core";
+import ThemeContext from "./ThemeContext";
+import CssBaseline from "@material-ui/core/CssBaseline/CssBaseline";
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import Sidebar from "./components/Sidebar";
+import IndexPage from "./pages/IndexPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import NotFoundPage from "./pages/NotFoundPage";
+
+import './index.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            theme: 'light',
+            toggleTheme: () =>
+                this.setState({
+                    theme: this.state.theme === 'light' ? 'dark' : 'light'
+                })
+        };
+    }
+
+    render() {
+        const customTheme = createMuiTheme({
+            palette: {
+                type: this.state.theme,
+                primary: {
+                    main: '#00796b'
+                },
+                secondary: {
+                    main: '#607D8B'
+                }
+            }
+        });
+
+        return (
+            <ThemeContext.Provider value={this.state}>
+                <MuiThemeProvider theme={customTheme}>
+                    <CssBaseline />
+                    <Router basename={process.env.PUBLIC_URL}>
+                        <Sidebar>
+                            <Switch>
+                                <Route exact path="/" component={IndexPage} />
+                                <Route path="/portfolio" component={ProjectsPage} />
+                            </Switch>
+                        </Sidebar>
+                    </Router>
+                </MuiThemeProvider>
+            </ThemeContext.Provider>
+        );
+    }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
