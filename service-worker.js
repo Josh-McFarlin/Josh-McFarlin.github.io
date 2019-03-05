d22b34c8f12950b4bc7e4502341f0476b3b39a7e
Josh-McFarlin <jmcfarlin6@gatech.edu>
2019-03-04 19:17:55 -0500

    Attempt to fix service worker issue

---
+++ service-worker.js
@@ -0,0 +1,11 @@
+self.addEventListener('install', () => {
+    self.skipWaiting();
+
+    if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistrations().then((registrations) => {
+            for (let registration of registrations) {
+                registration.unregister();
+            }
+        });
+    }
+});
